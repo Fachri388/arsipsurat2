465710f2bd00f0963ebb8d7084fa7df00352beaa
fachri <fachri.ost75@gmail.com>
2023-03-11 14:28:50 +0800

    first coomit

---
+++ application/views/dashboard.php
@@ -0,0 +1,68 @@
+<div id="page-wrapper">
+				<div class="graphs">
+					<h3 class="blank1">DASHBOARD</h3>
+					 <div class="xs tabls">
+						<div class="bs-example4" data-example-id="contextual-table">
+							    <div class="row">
+        </div>
+<div id="page-wrapper">
+				<div class="graphs">
+					<div class="col_3">
+						<div class="col-md-3 widget widget1">
+							<div class="r3_counter_box">
+								<i class="fa fa-mail-forward"></i>
+								<div class="stats">
+						
+								  	<div class="huge"><?php echo $data_dashboard['surat_masuk']; ?></div>
+									<p>Surat Masuk</p>
+								  
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 widget widget1">
+							<div class="r3_counter_box">
+								<i class="fa fa-users"></i>
+								<div class="stats">
+								  
+								  <div class="huge"><?php echo $data_dashboard['surat_keluar']; ?></div>
+									<p> Surat Keluar</p>
+								  </div>
+								</div>
+							</div>
+
+							<!-- <div class="col-md-3 widget widget1">
+							<div class="r3_counter_box">
+								<i class="fa fa-envelope"></i>
+								<div class="stats">
+								  
+								  <div class="huge"><?php echo $data_dashboard['surat_masuk']; ?></div>
+									<p> Surat disposisi</p>
+								  </div>
+								</div>
+							</div> -->
+
+							<div class="col-md-3 widget widget1">
+							<div class="r3_counter_box">
+								<i class="fa fa-user"></i>
+								<div class="stats">
+								  
+								  <div class="huge"></div>
+								  <p>1</p>
+								  <p>pengguna</p>
+								  </div>
+								</div>
+							</div>
+
+						</div>
+						
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+
+					
+
+
+		<!-- //switches -->
+	
+			
+		</div>
---
+++ database/arsipsurat.sql
@@ -0,0 +1,222 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Feb 2023 pada 06.25
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arsipsurat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `disposisi`
+--
+
+CREATE TABLE `disposisi` (
+  `id_disposisi` int(11) NOT NULL,
+  `id_surat` int(11) NOT NULL,
+  `id_pegawai_pengirim` int(11) NOT NULL,
+  `id_pegawai_penerima` int(11) NOT NULL,
+  `tgl_disposisi` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `disposisi`
+--
+
+INSERT INTO `disposisi` (`id_disposisi`, `id_surat`, `id_pegawai_pengirim`, `id_pegawai_penerima`, `tgl_disposisi`, `keterangan`) VALUES
+(14, 16, 1, 2, '2023-02-06 12:41:46', 'tolong di tindak lanjuti semestinya');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jabatan`
+--
+
+CREATE TABLE `jabatan` (
+  `id_jabatan` int(11) NOT NULL,
+  `nama_jabatan` varchar(100) NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `jabatan`
+--
+
+INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`, `level`) VALUES
+(1, 'Admin', 1),
+(2, 'User', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `id_pegawai` int(11) NOT NULL,
+  `user` varchar(60) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `id_jabatan` int(11) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pegawai`
+--
+
+INSERT INTO `pegawai` (`id_pegawai`, `user`, `nama`, `id_jabatan`, `password`) VALUES
+(1, 'admin', 'syarif', 1, 'admin123'),
+(2, 'pegawai', 'staff TU', 2, '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `surat_keluar`
+--
+
+CREATE TABLE `surat_keluar` (
+  `id_surat` int(11) NOT NULL,
+  `nomor_surat` int(11) NOT NULL,
+  `pengirim` varchar(100) NOT NULL,
+  `tgl_kirim` date NOT NULL,
+  `penerima` varchar(100) NOT NULL,
+  `perihal` varchar(100) NOT NULL,
+  `file_surat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `surat_masuk`
+--
+
+CREATE TABLE `surat_masuk` (
+  `id_surat` int(11) NOT NULL,
+  `nomor_surat` varchar(100) NOT NULL,
+  `tgl_kirim` date NOT NULL,
+  `tgl_terima` date NOT NULL,
+  `pengirim` varchar(200) NOT NULL,
+  `penerima` varchar(200) NOT NULL,
+  `perihal` varchar(100) NOT NULL,
+  `file_surat` text NOT NULL,
+  `status` enum('proses','selesai') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `surat_masuk`
+--
+
+INSERT INTO `surat_masuk` (`id_surat`, `nomor_surat`, `tgl_kirim`, `tgl_terima`, `pengirim`, `penerima`, `perihal`, `file_surat`, `status`) VALUES
+(16, '800/333/555', '2023-02-01', '2023-02-02', 'BKPSDM', 'admin', 'perihal magang', 'contoh.pdf', 'proses');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `disposisi`
+--
+ALTER TABLE `disposisi`
+  ADD PRIMARY KEY (`id_disposisi`),
+  ADD KEY `id_surat` (`id_surat`),
+  ADD KEY `id_pegawai_penerima` (`id_pegawai_penerima`);
+
+--
+-- Indeks untuk tabel `jabatan`
+--
+ALTER TABLE `jabatan`
+  ADD PRIMARY KEY (`id_jabatan`),
+  ADD KEY `id_jabatan` (`id_jabatan`);
+
+--
+-- Indeks untuk tabel `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`id_pegawai`),
+  ADD UNIQUE KEY `id_jabatan` (`id_jabatan`),
+  ADD KEY `id_pegawai` (`id_pegawai`);
+
+--
+-- Indeks untuk tabel `surat_keluar`
+--
+ALTER TABLE `surat_keluar`
+  ADD PRIMARY KEY (`id_surat`);
+
+--
+-- Indeks untuk tabel `surat_masuk`
+--
+ALTER TABLE `surat_masuk`
+  ADD PRIMARY KEY (`id_surat`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `disposisi`
+--
+ALTER TABLE `disposisi`
+  MODIFY `id_disposisi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `jabatan`
+--
+ALTER TABLE `jabatan`
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pegawai`
+--
+ALTER TABLE `pegawai`
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `surat_keluar`
+--
+ALTER TABLE `surat_keluar`
+  MODIFY `id_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `surat_masuk`
+--
+ALTER TABLE `surat_masuk`
+  MODIFY `id_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `disposisi`
+--
+ALTER TABLE `disposisi`
+  ADD CONSTRAINT `disposisi_ibfk_1` FOREIGN KEY (`id_pegawai_penerima`) REFERENCES `pegawai` (`id_pegawai`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `disposisi_ibfk_2` FOREIGN KEY (`id_surat`) REFERENCES `surat_masuk` (`id_surat`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD CONSTRAINT `pegawai_ibfk_1` FOREIGN KEY (`id_jabatan`) REFERENCES `jabatan` (`id_jabatan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
